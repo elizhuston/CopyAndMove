9424e15806e3ece0607e45a0673fd243c43164b6
Huston <n0270466@lm.lmig.com>
2017-06-09 10:09:08 -0400

    update

--- src/copyAndMove/CopyAndMove.java
+++ src/copyAndMove/CopyAndMove.java
@@ -5,6 +5,9 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class CopyAndMove {
 
@@ -16,6 +19,22 @@ public class CopyAndMove {
 
 	public static void write(byte[] bs, String outPath) {
 		// RandomAccessFile f = new RandomAccessFile(outPath, "w");
+
+		int fileIdx = (outPath.lastIndexOf("/") + 1);
+		String fileName = outPath.substring(fileIdx);
+		String dirPath = outPath.substring(0, fileIdx);
+		System.out.println(fileName);
+		System.out.println(dirPath);
+		Path path = Paths.get(dirPath);
+		if (!Files.exists(path)) {
+			try {
+				Files.createDirectories(path);
+			} catch (IOException ex) {
+				System.out.println("Exception creating directories " + dirPath);
+			}
+		}
+		System.out.println(path.toString());
+
 		try {
 			RandomAccessFile f = new RandomAccessFile(outPath, "rw");
 			f.write(bs);
@@ -23,6 +42,19 @@ public class CopyAndMove {
 		} catch (IOException ex) {
 			System.out.println(ex);
 		}
+	}
+
+	public static void delete(String filePath) {
+		Path fileToDeletePath = Paths.get(filePath);
+		System.out.println("Deleting file " + fileToDeletePath);
+		if (Files.exists(fileToDeletePath)) {
+			try {
+				Files.delete(fileToDeletePath);
+			} catch (IOException ex) {
+				System.out.println("Exception deleting file " + fileToDeletePath);
+			}
+		}
+
 	}
 
 	public static void read(String filePath) throws FileNotFoundException, IOException {
@@ -38,8 +70,9 @@ public class CopyAndMove {
 				data[dataIdx] = buf[i];
 				dataIdx++;
 			}
-			f.close();
+
 		}
+		f.close();
 		Charset cs = Charset.forName("UTF-8");
 		String s = new String(data, cs);
 		System.out.println(s);
@@ -48,20 +81,31 @@ public class CopyAndMove {
 	public static void main(String[] args) {
 		String fileIn = null;
 		String fileOut = null;
+		String command = "cp";
 
-		if (args.length == 2) {
-			fileIn = args[0];
-			fileOut = args[1];
-		}
-		else{ System.out.println("Please specify two parameters, Inputfile and OutputFile");}
-			
-		try {
-			CopyAndMove.read(fileIn);
-			CopyAndMove.write(data, fileOut);
-		} catch (FileNotFoundException ex) {
-			System.out.println("File was not found, please verify " + fileIn + " exists.");
-		} catch (IOException ex) {
+		if (args.length == 3) {
+			command =args[0];
+			System.out.println("command = " + command);
+			fileIn = args[1];
+			fileOut = args[2];
+
+			try {
+				CopyAndMove.read(fileIn);
+				CopyAndMove.write(data, fileOut);
+				System.out.println("command IS '" + command + "'");
+				if (command.equals("mv")) {
+					// delete the old file
+					System.out.println("command == mv" );
+					CopyAndMove.delete(fileIn);
+				}
+			} catch (FileNotFoundException ex) {
+				System.out.println("File was not found, please verify " + fileIn + " exists.");
+			} catch (IOException ex) {
 				System.out.println(ex);
+			}
+
+		} else {
+			System.out.println("Please specify three parameters, cp or mv, then Inputfile and OutputFile");
 		}
 	}
 }
